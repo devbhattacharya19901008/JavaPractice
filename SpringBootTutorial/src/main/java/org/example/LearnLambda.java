4ac7a9a29cb74546bdcd661263fa9a34ccdddae8
Dev Bhattacharya <dev_bhattacharya1990@yahoo.com>
2020-10-09 13:22:01 -0300

    Code for Lambda Interface

--- SpringBootTutorial/src/main/java/org/example/LearnLambda.java
+++ SpringBootTutorial/src/main/java/org/example/LearnLambda.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.function.Consumer;
 
 public class LearnLambda {
+
     public static void main(String args[]){
         LearnLambda learnLambda = new LearnLambda();
         ArrayList<Integer> arrayList = new ArrayList<>();
@@ -29,9 +30,24 @@ public class LearnLambda {
         String stringToDisplay = "Hello";
         System.out.println(exclaim.run(stringToDisplay));
         System.out.println(ask.run(stringToDisplay));
+
+
+        Operation.FuncInter1 add = (int a, int b) -> a + b;
+        Operation.FuncInter1 subtract = (int a, int b) -> a - b;
+
+        int addValue = learnLambda.operation(5, 7, add);
+        int subtractValue = learnLambda.operation(7, 5, subtract);
+
+        System.out.println(addValue + "  " + subtractValue);
+
+
     }
 
     void display(Integer n){
         System.out.println(n);
+    }
+
+    int operation(int a, int b, Operation.FuncInter1 funcInter1){
+        return funcInter1.operation(a, b);
     }
 }
---
+++ SpringBootTutorial/src/main/java/org/example/Operation.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Operation {
+    interface FuncInter1
+    {
+        int operation(int a, int b);
+    }
+}
